d6f437634aa7bcc6777f104d28e11509b3c709d0
hekla <hst10@hi.is>
2025-04-11 14:09:50 +0000

    laga db og texta

--- backend/init_db.sql
+++ backend/init_db.sql
@@ -8,36 +8,52 @@ CREATE TABLE trips (
   link VARCHAR(500),
   riding_days INTEGER,
   difficulty VARCHAR(255) NOT NULL,
-  availability INTEGER default 1
+  availability INTEGER 
 );
 
 DELETE from trips;
 ALTER SEQUENCE trips_id_seq RESTART WITH 1;
 
-INSERT INTO trips (title, start_date, end_date, link, riding_days, difficulty)
+INSERT INTO trips (title, start_date, end_date, link, riding_days, difficulty, availability)
 VALUES  
-  ('Chasing Waterfalls', '2025-06-14', '2025-06-20', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate'),  /*seinasti dagur er ekki included*/
-  ('Chasing Waterfalls', '2025-06-25', '2025-07-01', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/', 5, 'intermediate'), 
-  ('Beach and Lava', '2025-06-16', '2025-06-19', 'https://www.ishestar.is/packages/beach-lava-ride/', 3, 'intermediate'),
-  ('Kirkjufell', '2025-06-13', '2025-06-17', 'https://www.ishestar.is/packages/kirkjufell-riding-adventure/', 4, 'intermediate'),
-  ('Löngufjörur', '2025-06-17', '2025-06-19', 'https://www.ishestar.is/packages/longufjorur-beach-ride/', 2, 'intermediate'),
-  ('Snæfellsnes', '2025-06-21', '2025-06-30', 'https://www.ishestar.is/packages/snaefellsnes/', 5, 'intermediate'),
-  ('Chasing Waterfalls', '2025-07-08', '2025-07-14', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate'),
-  ('Chasing Waterfalls', '2025-07-20', '2025-07-26', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate'),
-  ('Hekla Adventure', '2025-07-14', '2025-07-19', 'https://www.ishestar.is/packages/hekla-adventure-2/',5, 'intermediate'),
-  ('Landmannalaugar Adventure', '2025-07-01', '2025-07-07', 'https://www.ishestar.is/packages/landmannalaugar-adventure/',6, 'intermediate'),
-  ('New Frontiers', '2025-07-10', '2025-07-18', 'https://www.ishestar.is/packages/new-frontier/',8, 'advanced'),
-  ('Beyond Kjölur', '2025-07-22', '2025-07-31', 'https://www.ishestar.is/packages/beyond-kjolur-off-the-beaten-path/',8, 'advanced'),
-  ('Chasing Waterfalls', '2025-08-15', '2025-08-21', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate'),
-  ('Black Beach', '2025-08-21', '2025-08-23', 'https://www.ishestar.is/packages/black-sand-beach-ride/',2, 'intermediate'),
-  ('Landmannalaugar Adventure', '2025-08-23', '2025-08-29', 'https://www.ishestar.is/packages/landmannalaugar-adventure/',6, 'intermediate'),
-  ('Hekla Adventure', '2025-08-08', '2025-08-13', 'https://www.ishestar.is/packages/hekla-adventure-2/',5, 'intermediate'),
-  ('Highland Express', '2025-08-06', '2025-08-09', 'https://www.ishestar.is/packages/highland-express-2/',4, 'intermediate'),
-  ('Black Sand Autumn Special', '2025-09-01', '2025-09-06', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',1, 'intermediate'),
-  ('Sheep Round-Up', '2025-09-16', '2025-09-21', 'https://www.ishestar.is/packages/sheep-round-up-reydarvatnsrettir/',4, 'intermediate'),
-  ('Into the far east', '2025-07-17', '2025-07-24', 'https://www.ishestar.is/packages/into-the-far-east/',7, 'advanced'), 
-  ('Midsummer in the Eastfjords', '2025-06-22', '2025-06-29', 'https://www.ishestar.is/packages/midsummer-in-the-eastfjords/',7, 'advanced'),
-  ('Ride to Vopnafjörður', '2025-08-23', '2025-09-01', 'https://www.ishestar.is/packages/ride-to-vopnafjordur/',9, 'advanced')
+  ('Midsummer Beach and Mountain', '2025-06-21', '2025-06-26', 'https://www.ishestar.is/packages/midsummer-beach-mountain/',5, 'intermediate',1),
+  ('New Frontier', '2025-07-10', '2025-07-18', 'https://www.ishestar.is/packages/new-frontier/',8, 'advanced',1),
+  ('Hekla Adventure', '2025-06-07', '2025-06-12', 'https://www.ishestar.is/packages/hekla-adventure-2/',5, 'intermediate',0),
+  ('Hekla Adventure', '2025-06-19', '2025-06-24', 'https://www.ishestar.is/packages/hekla-adventure-2/',5, 'intermediate',0),
+  ('Hekla Adventure', '2025-07-14', '2025-07-19', 'https://www.ishestar.is/packages/hekla-adventure-2/',5, 'intermediate',1),
+  ('Hekla Adventure', '2025-08-08', '2025-08-13', 'https://www.ishestar.is/packages/hekla-adventure-2/',5, 'intermediate',1),
+  ('Beyond Kjölur', '2025-07-22', '2025-07-31', 'https://www.ishestar.is/packages/beyond-kjolur-off-the-beaten-path/',8, 'advanced',1),
+   ('Highland Express', '2025-08-06', '2025-08-09', 'https://www.ishestar.is/packages/highland-express-2/',4, 'intermediate',1),
+('Magical Snæfellsnes', '2025-06-21', '2025-06-26', 'https://www.ishestar.is/packages/snaefellsnes/', 5, 'intermediate',0),
+('Magical Snæfellsnes', '2025-06-26', '2025-07-1', 'https://www.ishestar.is/packages/snaefellsnes/', 5, 'intermediate',1),
+('Landmannalaugar Adventure', '2025-07-01', '2025-07-07', 'https://www.ishestar.is/packages/landmannalaugar-adventure/',6, 'intermediate',0),
+('Landmannalaugar Adventure', '2025-07-26', '2025-08-01', 'https://www.ishestar.is/packages/landmannalaugar-adventure/',6, 'intermediate',1),
+('Landmannalaugar Adventure', '2025-08-23', '2025-08-29', 'https://www.ishestar.is/packages/landmannalaugar-adventure/',6, 'intermediate',1),
+('Kirkjufell Adventure', '2025-06-13', '2025-06-17', 'https://www.ishestar.is/packages/kirkjufell-riding-adventure/', 4, 'intermediate',0),
+ ('Chasing Waterfalls', '2025-06-14', '2025-06-20', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate',1),  /*seinasti dagur er ekki included*/
+  ('Chasing Waterfalls', '2025-06-25', '2025-07-01', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/', 5, 'intermediate',1), 
+('Chasing Waterfalls', '2025-07-08', '2025-07-14', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate',0),
+  ('Chasing Waterfalls', '2025-07-20', '2025-07-26', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate',0),
+  ('Chasing Waterfalls', '2025-08-15', '2025-08-21', 'https://www.ishestar.is/packages/chasing-waterfalls-foss-comfort-tour/',5, 'intermediate',0),
+('Midsummer in the Eastfjords', '2025-06-22', '2025-06-29', 'https://www.ishestar.is/packages/midsummer-in-the-eastfjords/',7, 'advanced',1),
+('Löngufjörur Beach Ride', '2025-06-17', '2025-06-19', 'https://www.ishestar.is/packages/longufjorur-beach-ride/', 2, 'intermediate',1),
+ ('Black Sand Beach Ride', '2025-08-21', '2025-08-23', 'https://www.ishestar.is/packages/black-sand-beach-ride/',2, 'intermediate',1),
+  ('Beach and Lava Ride', '2025-06-16', '2025-06-19', 'https://www.ishestar.is/packages/beach-lava-ride/', 3, 'intermediate',1),
+    ('Sheep Roundups', '2025-09-16', '2025-09-21', 'https://www.ishestar.is/packages/sheep-round-up-reydarvatnsrettir/',4, 'intermediate',1),
+    ('Horse Roundups', '2025-09-04', '2025-09-08', 'https://www.ishestar.is/packages/horse-roundups/',5, 'advanced',1),
+     ('Horse Roundups', '2025-09-09', '2025-09-13', 'https://www.ishestar.is/packages/horse-roundups/',5, 'advanced',0),
+   ('Horse Roundups', '2025-09-17', '2025-09-21', 'https://www.ishestar.is/packages/horse-roundups/',5, 'advanced',1),
+   ('Horse Roundups', '2025-09-23', '2025-09-27', 'https://www.ishestar.is/packages/horse-roundups/',5, 'advanced',1),
+   ('Horse Roundups', '2025-10-01', '2025-10-05', 'https://www.ishestar.is/packages/horse-roundups/',5, 'advanced',0),
+   ('Saltvík Comfort', '2025-05-08', '2025-05-11', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',0),
+  ('Saltvík Comfort', '2025-05-26', '2025-05-29', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',0),
+  ('Saltvík Comfort', '2025-06-13', '2025-06-16', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',0),
+  ('Saltvík Comfort', '2025-07-06', '2025-07-09', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',1),
+  ('Saltvík Comfort', '2025-07-19', '2025-07-22', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',0),
+  ('Saltvík Comfort', '2025-08-09', '2025-08-12', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',0),
+  ('Saltvík Comfort', '2025-09-01', '2025-09-04', 'https://www.ishestar.is/packages/saltvikcomfort/',3, 'intermediate',1),
+  ('Into the far east', '2025-07-17', '2025-07-24', 'https://www.ishestar.is/packages/into-the-far-east/',7, 'advanced',0), 
+  ('Ride to Vopnafjörður', '2025-08-23', '2025-09-01', 'https://www.ishestar.is/packages/ride-to-vopnafjordur/',9, 'advanced',0)
   ;
 
 SELECT * from trips;
--- backend/tripNameMap.js
+++ backend/tripNameMap.js
@@ -1,7 +1,20 @@
 //úr rezdy nafni í database nafn
 const tripNameMap = {
     "Chasing Waterfalls - From Summit to Sea": "Chasing Waterfalls",
-    "Northern Explorer": "North Mountain Trail",
-    "Golden Circle Ride": "Golden Adventure Tour",
+    "Midsummer Beach and Mountain": "Midsummer Beach & Mountain",
+    "New Frontier": "New Frontier",
+    "Hekla Adventure":"Hekla Adventure",
+    "Beyond Kjölur": "Beyond Kjölur",
+    "Highland Express": "Highland Express",
+    "Snaefellsnes - Magical and mysterious": "Magical Snæfellsnes",
+    "Landmannalaugar Adventure": "Landmannalaugar Adventure",
+    "Kirkjufell Adventure": "Kirkjufell Adventure",
+    "Midsummer in the Eastfjords": "Midsummer in the Eastfjords",
+    "Löngufjörur beach ride":  "Löngufjörur Beach Ride",
+    "Black Sand Beach Ride": "Black Sand Beach Ride",
+    "Beach & Lava Ride": "Beach and Lava Ride",
+    "Sheep round up - Reyðarvatnsrettir":"Sheep Round-Up",
+    //VANTAR SALTVIK OG HORSE ROUNDUPS, 
+    // DOUBLE CHECKA Á ÖLLUM SEM ERU EKKIEINS, CPY PASTE ÚR BROWSER REZDY NAFN
 };
 module.exports = { tripNameMap };
